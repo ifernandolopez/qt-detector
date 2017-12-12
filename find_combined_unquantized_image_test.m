43121feb927b3fd94a431b1804e57eb28a55562c
ifernandolopez <flopez@wifi-95-117.uc3m.es>
2017-12-12 10:42:02 +0100

    Tyding up the unquantized tests

--- find_combined_unquantized_image_test.m
+++ find_combined_unquantized_image_test.m
@@ -5,10 +5,10 @@ clear;
 % Path to CoMoFoD unquantized imageset
 paths = get_imageset_paths('/Volumes/autor/storage/datasets/quantization/*_O.png');
 
-for path_i = [1:length(paths)]
+for path_i = 1:length(paths)
   path = paths{path_i};
   M = get_image_blocks(path);
   % Always guess_q==100 as the unique solution with an unquantified image
-  guess_q = find_mle_qt_of_image(M);
+  guess_q = find_combined_qt_of_image(M);
   assert(guess_q==100);
 end
--- find_compatible_unquantized_image_test.m
+++ find_compatible_unquantized_image_test.m
@@ -5,14 +5,17 @@ clear;
 % Path to CoMoFoD unquantized imageset
 paths = get_imageset_paths('/Volumes/autor/storage/datasets/quantization/*_O.png');
 
-compatible_solutions = 0;
+compatible_solutions = zeros(1,length(paths));
 
-for path_i = [1:length(paths)];
+for path_i = 1:length(paths)
   path = paths{path_i};
   M = get_image_blocks(path);
   % Find guess_q==100 as one of the solutions with an unquantified image
   guess_q = find_compatible_qt_of_image(M);
-  % Count the number of compatible solutions
-  compatible_solutions = compatible_solutions + length(guess_q);
+  % Store the number of compatible solutions for each path_i
+  compatible_solutions(path_i) = length(guess_q);
   assert(ismember(100,guess_q));
 end
+
+mean(compatible_solutions)
+std(compatible_solutions)
